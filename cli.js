9f18e9a1576421c37dbd655b5be1c0bf7c69e97d
tagpi <david@tagpi.com>
2021-03-31 21:35:19 -0400

    Update cli

--- cli.js
+++ cli.js
@@ -1,14 +1,1 @@
-
-const command = process.argv[2];
-switch (command) {
-
-  case 'start':
-    let configParam = process.argv[3];
-    exports.serve(configParam);
-    break;
-
-  default:
-    console.log('[@tagpi/node-quick-serve]'.yellow, 'invalid command'.red);
-    break;
-
-}
+require('./index.js').serve(process.argv[2]);
